31a0cbb5da7ede4dd74a62126856fe3aca5fe05c
Natalie Clark <nmclark2@iastate.edu>
2020-03-27 22:24:18 -0500

    Add files via upload

--- SCION_no_hubs.R
+++ SCION_no_hubs.R
@@ -11,46 +11,48 @@
 #For data tables, columns are samples are rows are genes. 
 #Data are transposed before running the GENIE3 step. 
 
-SCION <- function(target_genes_file,reg_genes_file,target_data_file,reg_data_file,clustering_data_file,threshold,working_dir){
+SCION_no_hubs <- function(target_genes_file,reg_genes_file,target_data_file,reg_data_file,clustering_data_file){
   
-  #check that all packages are installed, and load them
-  for (package in c('dtwclust', 'plotly','randomForest')) {
-    if (!require(package, character.only=T, quietly=T)) {
-      install.packages(package)
-      library(package, character.only=T)
-    }else{
-      library(package,character.only=T)
-    }
-  }
+  # #check that all packages are installed, and load them
+  # for (package in c('dtwclust', 'plotly','randomForest')) {
+  #   if (!require(package, character.only=T, quietly=T)) {
+  #     install.packages(package)
+  #     library(package, character.only=T)
+  #   }else{
+  #     library(package,character.only=T)
+  #   }
+  # }
+  library(randomForest)
   
   #load functions and set working directory
   #make sure the functions are in your working directory!
-  setwd(working_dir)
+  #setwd(working_dir)
   
   #make folders we need
-  dir.create('Cluster Plots')
-  dir.create('Cluster networks')
+  #dir.create('Cluster Plots')
+  #dir.create('Cluster networks')
 
   #read in tables
   target_genes = read.csv(target_genes_file,stringsAsFactors=FALSE)
   reg_genes = read.csv(reg_genes_file,stringsAsFactors=FALSE)
   target_data = read.csv(target_data_file,row.names=1)
   reg_data = read.csv(reg_data_file,row.names=1)
-  clustering_data = read.csv(clustering_data_file,row.names=1)
+  #clustering_data = read.csv(clustering_data_file,row.names=1)
   
   #get data for targets and regulators
   mytargetdata = target_data[row.names(target_data)%in%target_genes[,1],]
   myregdata = reg_data[row.names(reg_data)%in%reg_genes[,1],]
   
   #get clustering data
-  myclustdata = clustering_data[row.names(clustering_data)%in%target_genes[,1],]
+  #myclustdata = clustering_data[row.names(clustering_data)%in%target_genes[,1],]
   
   #run clustering first
-  clusterresults <- dtw_clustering(myclustdata,threshold)
+  #clusterresults <- dtw_clustering(myclustdata,threshold)
+  clusterresults <- read.csv(clustering_data_file,stringsAsFactors=FALSE)
   
   #infer a network using GENIE3 on each cluster
-  for (i in 1:max(clusterresults$clusters)){
-    mygenes = row.names(clusterresults[clusterresults$clusters==i,])
+  for (i in 1:max(clusterresults$Cluster)){
+    mygenes = clusterresults[clusterresults$Cluster==i,1]
     clustertargetdata = mytargetdata[row.names(mytargetdata)%in%mygenes,]
     clusterregdata = myregdata[row.names(myregdata)%in%mygenes,]
     #if there's less than 2 TFs we skip the cluster
@@ -58,8 +60,12 @@ SCION <- function(target_genes_file,reg_genes_file,target_data_file,reg_data_fil
     if (dim(clusterregdata)[1]<=2){
       next
     }
+    #if there are no targets, we also skip the cluster
+    if (dim(clustertargetdata)[1]<1){
+      next
+    }
     
-    network = RS.Get.Weight.Matrix(t(clustertargetdata),t(clusterregdata))
+    network = RS.Get.Weight.Matrix_cluster(t(clustertargetdata),t(clusterregdata))
     
     #trim matrix based on the ratio of TFs to targets - more TFs per target = more edges kept
     TFratio = (dim(clusterregdata)[1])/(dim(clustertargetdata)[1]+dim(clusterregdata)[1])
@@ -109,69 +115,69 @@ SCION <- function(target_genes_file,reg_genes_file,target_data_file,reg_data_fil
     #write table to file
     write.table(networktable,paste('Cluster networks/network_cluster_',i,'.txt',sep=""),row.names=FALSE,quote=FALSE,sep='\t')
     
-    #get the hub gene (highest outdegree) and save it to connect the clusters later
-    #if there is a tie, we save both hubs
-    myregs = unique(networktable$Regulator)
-    for (j in 1:length(myregs)){
-      numedges = sum(networktable$Regulator%in%myregs[j])
-      if (j==1){
-        hubedges = numedges
-        hub = myregs[j]
-      }else if (numedges>hubedges){
-        hubedges = numedges
-        hub = myregs[j]
-      }else if (numedges==hubedges){
-        hub = rbind(hub,myregs[j])
-      }
-    }
-    if (!exists("myhubs")){
-      myhubs = hub
-    } else{
-      myhubs = rbind(myhubs,hub)
-    }
+    # #get the hub gene (highest outdegree) and save it to connect the clusters later
+    # #if there is a tie, we save both hubs
+    # myregs = unique(networktable$Regulator)
+    # for (j in 1:length(myregs)){
+    #   numedges = sum(networktable$Regulator%in%myregs[j])
+    #   if (j==1){
+    #     hubedges = numedges
+    #     hub = myregs[j]
+    #   }else if (numedges>hubedges){
+    #     hubedges = numedges
+    #     hub = myregs[j]
+    #   }else if (numedges==hubedges){
+    #     hub = rbind(hub,myregs[j])
+    #   }
+    # }
+    # if (!exists("myhubs")){
+    #   myhubs = hub
+    # } else{
+    #   myhubs = rbind(myhubs,hub)
+    # }
   }
-  #infer network connecting the hubs
-  #as before we can only do this if there is more than 1 hub
-  if (exists("myhubs") && length(myhubs)>2){
-    hubtargetdata = mytargetdata[row.names(mytargetdata)%in%myhubs,]
-    hubregdata = myregdata[row.names(myregdata)%in%myhubs,]
-    network = RS.Get.Weight.Matrix(t(hubtargetdata),t(hubregdata))
-    
-    #trim matrix based on the ratio of TFs to targets - more TFs per target = more edges kept
-    edgestokeep = floor(4*dim(hubtargetdata)[1])
-    
-    #get the weight threshold that corresponds to this number of edges to keep
-    allweights = stack(data.frame(network))
-    allweights = allweights[order(allweights[,1],decreasing=TRUE),1]
-    weightthreshold = allweights[edgestokeep]
-    
-    #now make a new network where we eliminate all the low confidence edges
-    trimmednet = data.frame(ifelse(network<weightthreshold,NaN,network))
-    
-    #translate the trimmed network into a table we can import into cytoscape
-    networktable = data.frame(Regulator=character(), Interaction=character(), Target=character(), Weight=double(),
-                              stringsAsFactors=FALSE)
-    row=1
-    for (j in 1:dim(trimmednet)[1]){
-      for (k in 1:dim(trimmednet)[2]){
-        #skip NaNs as these have no edge
-        if (is.na(trimmednet[j,k])){
-          next
-        }else{
-          networktable[row,] = cbind(colnames(trimmednet)[k],"regulates",rownames(trimmednet)[j],trimmednet[j,k])
-          row = row+1
-        }
-      }
-    }
-    
-    #if there are . in the networktable, replace them with - (for phospho)
-    if (length(grepl('\\.',networktable$Regulator))>0){
-      networktable$Regulator <- sapply(networktable$Regulator, function(x) gsub("\\.", "-", x))
-    }
-    
-    #write table to file
-    write.table(networktable,'Cluster networks/network_hub.txt',row.names=FALSE,quote=FALSE,sep='\t')
-  }
+  # #infer network connecting the hubs
+  # #as before we can only do this if there is more than 1 hub
+  # if (exists("myhubs") && length(myhubs)>2){
+  #   hubtargetdata = mytargetdata[row.names(mytargetdata)%in%myhubs,]
+  #   hubregdata = myregdata[row.names(myregdata)%in%myhubs,]
+  #   network = RS.Get.Weight.Matrix_cluster(t(hubtargetdata),t(hubregdata))
+  #   
+  #   #trim matrix based on the ratio of TFs to targets - more TFs per target = more edges kept
+  #   edgestokeep = floor(4*dim(hubtargetdata)[1])
+  #   
+  #   #get the weight threshold that corresponds to this number of edges to keep
+  #   allweights = stack(data.frame(network))
+  #   allweights = allweights[order(allweights[,1],decreasing=TRUE),1]
+  #   weightthreshold = allweights[edgestokeep]
+  #   
+  #   #now make a new network where we eliminate all the low confidence edges
+  #   trimmednet = data.frame(ifelse(network<weightthreshold,NaN,network))
+  #   
+  #   #translate the trimmed network into a table we can import into cytoscape
+  #   networktable = data.frame(Regulator=character(), Interaction=character(), Target=character(), Weight=double(),
+  #                             stringsAsFactors=FALSE)
+  #   row=1
+  #   for (j in 1:dim(trimmednet)[1]){
+  #     for (k in 1:dim(trimmednet)[2]){
+  #       #skip NaNs as these have no edge
+  #       if (is.na(trimmednet[j,k])){
+  #         next
+  #       }else{
+  #         networktable[row,] = cbind(colnames(trimmednet)[k],"regulates",rownames(trimmednet)[j],trimmednet[j,k])
+  #         row = row+1
+  #       }
+  #     }
+  #   }
+  #   
+  #   #if there are . in the networktable, replace them with - (for phospho)
+  #   if (length(grepl('\\.',networktable$Regulator))>0){
+  #     networktable$Regulator <- sapply(networktable$Regulator, function(x) gsub("\\.", "-", x))
+  #   }
+  #   
+  #   #write table to file
+  #   write.table(networktable,'Cluster networks/network_hub.txt',row.names=FALSE,quote=FALSE,sep='\t')
+  # }
   
   #read in all the tables, combine, and save as the final file to import into cytoscape
   #get all of the folders for our samples
