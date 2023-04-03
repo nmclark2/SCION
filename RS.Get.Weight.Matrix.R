3a6605d1f1e71f9bff7db4e598ca36db5e1ca06a
Natalie Clark <nclark@broadinstitute.org>
2023-04-03 16:23:59 -0400

    Version 4.0
    - Adds option for k-means clustering (non-temporal data)
    - Adds option to not normalize edge weights
    - Adds option for parallelization

--- RS.Get.Weight.Matrix.R
+++ RS.Get.Weight.Matrix.R
@@ -1,5 +1,5 @@
 # target.matrix and input.matrix (TFs) have samples as rows and genes as columns
-RS.Get.Weight.Matrix<- function(target.matrix, input.matrix, K="sqrt", nb.trees=10000, importance.measure="%IncMSE", seed=NULL, trace=TRUE, ...)  
+RS.Get.Weight.Matrix<- function(target.matrix, input.matrix, K="sqrt", nb.trees=10000, importance.measure="%IncMSE", seed=NULL, trace=TRUE, normalize=TRUE, num.cores=1, ...)  
 {
   #require(parallel)
   # set random number generator seed if seed is given
@@ -41,23 +41,28 @@ RS.Get.Weight.Matrix<- function(target.matrix, input.matrix, K="sqrt", nb.trees=
   } else {
     stop("Parameter K must be \"sqrt\", or \"all\", or an integer")
   }
-  if (trace) {
-    cat(paste("Starting RF computations with ", nb.trees,
-              " trees/target gene,\nand ", mtry,
-              " candidate input genes/tree node\n",
-              sep=""))
-    flush.console()
-  }
+  # if (trace) {
+  #   cat(paste("Starting RF computations with ", nb.trees,
+  #             " trees/target gene,\nand ", mtry,
+  #             " candidate input genes/tree node\n",
+  #             sep=""))
+  #   flush.console()
+  # }
   
   # compute importances for every target gene
   names(target.names)<-target.names
   #return(list(target.names,input.matrix,target.matrix))
-  #clst<-makeCluster(getOption("cl.cores",4))
-  #clusterExport(cl = clst, varlist = c("importance","randomForest","RSGWM2","num.targets","target.names","input.matrix","target.matrix","trace","mtry","nb.trees","importance.measure",...),envir = environment())
-  #imList<-parLapplyLB(cl=clst, X=target.names, function(x) RSGWM2(x,num.targets,target.names,input.matrix,target.matrix,trace,mtry,nb.trees,importance.measure,...))
-  #stopCluster(cl=clst)
   
-  imList<-lapply(target.names,function(x) RSGWM2(x,num.targets,target.names,input.matrix,target.matrix,trace,mtry,nb.trees,importance.measure,...))
+  #parallelize if at least 3 cores, otherwise, don't
+  if(num.cores>2){
+    clst <- makeCluster(num.cores-1,type="FORK",outfile="log.txt")
+    registerDoParallel(clst)
+    imList<-parLapply(cl=clst, X=target.names, function(x) RSGWM2(x,num.targets,target.names,input.matrix,target.matrix,trace,mtry,nb.trees,importance.measure,...))
+    stopCluster(cl=clst)
+  }else{
+    imList<-lapply(target.names,function(x) RSGWM2(x,num.targets,target.names,input.matrix,target.matrix,trace,mtry,nb.trees,importance.measure,...))
+  }
+  
   #return(imList)
   for(nm in names(imList))
   {
@@ -67,7 +72,9 @@ RS.Get.Weight.Matrix<- function(target.matrix, input.matrix, K="sqrt", nb.trees=
   # weight.matrix<-sapply(imList,function(x) x)
   
   mynet <- weight.matrix/num.samples
-  mynet <- (mynet-min(mynet,na.rm=TRUE))/(max(mynet,na.rm=TRUE)-min(mynet,na.rm=TRUE))
+  if(normalize==TRUE){
+    mynet <- (mynet-min(mynet,na.rm=TRUE))/(max(mynet,na.rm=TRUE)-min(mynet,na.rm=TRUE))
+  }
   return(mynet)
   #    return(list(Weight=weight.matrix,Model=model.matrix,PedictionCorrelations=cor.vec))
 }   
@@ -77,20 +84,25 @@ RSGWM2<-function(target.gene.name,num.targets,target.names,input.matrix,target.m
   target.gene.idx<-which(target.names==target.gene.name)
   if (trace) 
   {
-    cat(paste("Computing gene ", target.gene.idx, "/", num.targets, "\n", sep=""))
+    #cat(paste("Computing gene ", target.gene.idx, "/", num.targets, "\n", sep=""))
     flush.console()
   }
   #target.gene.name <- target.names[target.gene.idx]
   # remove target gene from input genes
   #these.input.gene.names <- setdiff(input.gene.names, target.gene.name)
   temp.input.matrix<-input.matrix
-  if(target.gene.name %in% colnames(input.matrix))
-  {
-    rmind<-which(colnames(input.matrix)==target.gene.name)
-    #rmind<-grep(target.gene.name,colnames(input.matrix),fixed=T)
-    temp.input.matrix<-input.matrix[,-rmind]
-    print(paste("Removing:",target.gene.name,"fom Input| Index Number:",rmind," |  New Dimensions:",dim(temp.input.matrix)[1],"X",dim(temp.input.matrix)[2]))
-  }
+  
+  #do NOT remove the gene from the input matrix
+  #this breaks the code when there is only 1 regulator in a network
+  #I believe this was originally written to remove autoregulation from the network
+  
+  # if(target.gene.name %in% colnames(input.matrix))
+  # {
+  #   rmind<-which(colnames(input.matrix)==target.gene.name)
+  #   #rmind<-grep(target.gene.name,colnames(input.matrix),fixed=T)
+  #   temp.input.matrix<-input.matrix[,-rmind]
+  #   #print(paste("Removing:",target.gene.name,"fom Input | Index Number:",rmind," |  New Dimensions:",dim(temp.input.matrix)[1],"X",dim(temp.input.matrix)[2]))
+  # }
   x <- temp.input.matrix
   y <- target.matrix[,target.gene.name]
   #incSamp<-names(y)[which(!is.na(y))]
@@ -98,8 +110,9 @@ RSGWM2<-function(target.gene.name,num.targets,target.names,input.matrix,target.m
   #y<-y[incSamp]
   
   rf <- randomForest(x = x, y = y, mtry=mtry, ntree=nb.trees, keep.forest=F, importance=TRUE,...)
-  im <- importance(rf)[,importance.measure]
-
-  #im.names <- names(im)
-  return(im)
+  
+    im <- importance(rf)[,importance.measure]
+    
+    #im.names <- names(im)
+    return(im)
 }
